89aa0b6836324b7b0e866e2706259f3d1e84cf9b
5555dfdf <3123048821@qq.com>
2026-04-13 15:35:13 +0800

    4.13 添加新增，修改，删除专长功能

--- src/main/java/org/example/coursework3/service/AdminService.java
+++ src/main/java/org/example/coursework3/service/AdminService.java
@@ -23,67 +23,63 @@ public class AdminService {
     private final ExpertiseRepository expertiseRepository;
 
 @Transactional
-public Specialist createSpecialist(CreateSpecialistRequest request) {
-    if (userRepository.findByEmail(request.getUserEmail()).isPresent()) {
-        throw new MsgException("该邮箱已被注册");
-    }
+    public Specialist createSpecialist(CreateSpecialistRequest request) {
+        if (userRepository.findByEmail(request.getUserEmail()).isPresent()) {
+            throw new MsgException("该邮箱已被注册");
+        }
 
-    // 1. 保存用户
-    User user = new User();
-    user.setName(request.getName());
-    user.setEmail(request.getUserEmail());
-    user.setRole(Role.Specialist);
-    user.setPasswordHash(request.getPassword());
-    userRepository.save(user);
+        // 1. 保存用户
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getUserEmail());
+        user.setRole(Role.Specialist);
+        user.setPasswordHash(request.getPassword());
+        userRepository.save(user);
 
-    // 2. 创建专家（userId = user.getId()）
-    Specialist specialist = new Specialist(user.getId(), user.getName(), request.getPrice(), request.getBio());
-    List<Expertise> expertiseList = new ArrayList<>();
-    for (String expertiseId : request.getExpertiseIds()) {
-        Expertise expertise = expertiseRepository.findById(expertiseId)
-                .orElseThrow(() -> new MsgException("专长不存在"));
-        expertiseList.add(expertise);
-    }
-    specialist.setExpertises(expertiseList);
-    specialistsRepository.save(specialist); // JPA 自动维护中间表specialist_expertise
-
-
+        // 2. 创建专家（userId = user.getId()）
+        Specialist specialist = new Specialist(user.getId(), user.getName(), request.getPrice(), request.getBio());
+        List<Expertise> expertiseList = new ArrayList<>();
+        for (String expertiseId : request.getExpertiseIds()) {
+            Expertise expertise = expertiseRepository.findById(expertiseId)
+                    .orElseThrow(() -> new MsgException("专长不存在"));
+            expertiseList.add(expertise);
+        }
+        specialist.setExpertises(expertiseList);
+        specialistsRepository.save(specialist); // JPA 自动维护中间表specialist_expertise
     return specialist;
 }
     public Specialist updateSpecialist(String id, EditSpecialistRequest request) {
-    Specialist specialist;
-        try {
-            specialist = specialistsRepository.getByUserId(id);
-        } catch (Exception e) {
-            throw new MsgException("该专家不存在");
-        }
-        if (request.getName() != null) {
-            User user = userRepository.findById(id);
-            user.setName(request.getName());
-            userRepository.save(user);
-            specialist.setName(request.getName());
-        }
-        if (request.getBio() != null) {
-            specialist.setBio(request.getBio());
-        }
-        if (request.getPrice() != null) {
-            specialist.setPrice(request.getPrice());
-        }
-        if (request.getStatus() != null) {
-            specialist.setStatus(request.getStatus());
-        }
-        if (request.getExpertiseIds() != null){
-            List<Expertise> expertiseList = new ArrayList<>();
-            for (String expertiseId : request.getExpertiseIds()) {
-                Expertise expertise = expertiseRepository.findById(expertiseId)
-                        .orElseThrow(() -> new MsgException("专长不存在"));
-                expertiseList.add(expertise);
+        Specialist specialist;
+            try {
+                specialist = specialistsRepository.getByUserId(id);
+            } catch (Exception e) {
+                throw new MsgException("该专家不存在");
             }
-            specialist.setExpertises(expertiseList);
-        }
-        specialistsRepository.save(specialist);
-
-
+            if (request.getName() != null) {
+                User user = userRepository.findById(id);
+                user.setName(request.getName());
+                userRepository.save(user);
+                specialist.setName(request.getName());
+            }
+            if (request.getBio() != null) {
+                specialist.setBio(request.getBio());
+            }
+            if (request.getPrice() != null) {
+                specialist.setPrice(request.getPrice());
+            }
+            if (request.getStatus() != null) {
+                specialist.setStatus(request.getStatus());
+            }
+            if (request.getExpertiseIds() != null){
+                List<Expertise> expertiseList = new ArrayList<>();
+                for (String expertiseId : request.getExpertiseIds()) {
+                    Expertise expertise = expertiseRepository.findById(expertiseId)
+                            .orElseThrow(() -> new MsgException("专长不存在"));
+                    expertiseList.add(expertise);
+                }
+                specialist.setExpertises(expertiseList);
+            }
+            specialistsRepository.save(specialist);
     return specialist;
     }
 
@@ -102,20 +98,41 @@ public Specialist createSpecialist(CreateSpecialistRequest request) {
 
     @Transactional
     public void deleteSpecialist(String id) {
-    User user = userRepository.getUserById(id);
-    Specialist specialist = specialistsRepository.getByUserId(id);
-    specialistsRepository.delete(specialist);
-    userRepository.delete(user);
+        User user = userRepository.getUserById(id);
+        Specialist specialist = specialistsRepository.getByUserId(id);
+        specialistsRepository.delete(specialist);
+        userRepository.delete(user);
     }
 
     public Expertise createExpertise(String name, String description) {
-        return new Expertise();
+        if (expertiseRepository.existsByName(name)){
+            throw new MsgException("该专家长已存在");
+        }
+        Expertise expertise = new Expertise();
+        expertise.setName(name);
+        expertise.setDescription(description);
+        expertiseRepository.save(expertise);
+        return expertise;
     }
 
+    @Transactional
     public Expertise updateExpertise(String id, String name, String description) {
-        return new Expertise();
+        Expertise expertise = null;
+        try {
+            expertise = expertiseRepository.getExpertiseById(id);
+        } catch (Exception e) {
+            throw new MsgException("该专长不存在");
+        }
+        expertise.setName(name);
+        expertise.setDescription(description);
+        expertiseRepository.save(expertise);
+        return expertise;
     }
 
     public void deleteExpertise(String id) {
+        if (!expertiseRepository.existsById(id)){
+            throw new MsgException("请输入有效专长ID");
+        }
+        expertiseRepository.deleteById(id);
     }
 }
