9f693f6cd7c73e384761fc1783f3388a293921b5
5555dfdf <3123048821@qq.com>
2026-04-21 13:12:09 +0800

    4.21 新增自定义线程池 Bean

--- src/main/java/org/example/coursework3/config/AsyncConfig.java
+++ src/main/java/org/example/coursework3/config/AsyncConfig.java
@@ -1,9 +1,25 @@
 package org.example.coursework3.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @Configuration
 @EnableAsync
 public class AsyncConfig {
+
+    @Bean("taskExecutor")
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("async-");
+        System.out.println("1");
+        executor.initialize();
+        return executor;
+    }
 }
